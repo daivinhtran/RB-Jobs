9f44591cf0594bbc4364837462b08ae2771db426
Vinh Tran <daivinhtran@gmail.com>
2016-11-16 02:43:13 +0000

    remove token from script

--- updateGitInfo.py
+++ updateGitInfo.py
@@ -4,9 +4,17 @@ import requests
 import json
 from datetime import datetime
 
+import sys
+sys.path.append('~/Desktop/github/secret_tokens')
+
+import tokens
+
 events_url = "https://api.github.com/users/daivinhtran/events"
-response = requests.get(events_url, auth=('daivinhtran', '1b505b55b2715dbead2a8a5711c92be90590e9b3'))
-print(response.status_code)
+response = requests.get(events_url, auth=('daivinhtran', tokens.github_OAuth))
+
+if response.status_code != 200:
+	print(response.text)
+
 events = response.json();
 
 commitCount = 0;
@@ -14,9 +22,9 @@ start = None
 end = None
 
 for event in events:
+
     if event['type'] == 'PushEvent':
         date_obj = datetime.strptime(event['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-
         if start is None:
             start = date_obj
 
@@ -38,15 +46,15 @@ data = {'commit_count': commitCount, 'interval': interval.days}
 with open('../daivinhtran.github.io/_data/github_stats.json', 'w') as f:
   json.dump(data, f, ensure_ascii=False)
 
-path = "/Users/vinhtran/Desktop/daivinhtran.github.io"
+path = "../daivinhtran.github.io"
 os.chdir(path)
-
-# output = subprocess.check_output(["git", "status"])
-
+ 
 output = subprocess.check_output(["git", "status"])
+
 if "nothing to commit" in output:
-    subprocess.check_output(["git", "push", "origin", "master"])
+        subprocess.check_output(["git", "push", "origin", "master"])
+        print(output)
 else:
-    subprocess.check_output(["git", "add", "_data/github_stats.json"])
-    subprocess.check_output(["git", "commit", "-m", "update github_stats.json"])
-    subprocess.check_output(["git", "push", "origin", "master"])
+        subprocess.call(["git", "add", "_data/github_stats.json"])
+        subprocess.call(["git", "commit", "-m", "'update github_stats.json'"])
+        subprocess.call(["git", "push", "origin", "master"])
